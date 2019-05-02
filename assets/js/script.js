b3f6c89636cf29ea571592b3f62b6a510fd30d44
Sávio Andres <savioandres@hotmail.com>
2019-05-01 21:26:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,3 @@ $(function() {
     $("header").load("header.html"); 
     $("nav").load("nav.html"); 
 });
-
-$("nav a").click(function( e ) {
-    e.preventDefault();
-    var href = $( this ).attr('href');
-    $("#content").load( href +" #content");
-});
