1a78692800f31729c7828428b99d459da3d0b44b
Sávio Andres <savioandres@hotmail.com>
2019-05-01 21:21:02 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+$(function() {
+    $("header").load("header.html"); 
+    $("nav").load("nav.html"); 
+});
 
+$(document).ready(function() {
+    $("nav a").click(function( e ) {
+        e.preventDefault();
+        var href = $( this ).attr('href');
+        $("#content").load( href +" #content");
+    });
+});
